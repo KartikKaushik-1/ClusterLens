c3a43a9f7ff8ef7382b41bb553e61680550b69c8
KartikKaushik-1 <kartikaushik.1@gmail.com>
2025-11-10 19:54:13 +0530

    Update requirement.txt

--- Tools/openAI.py
+++ Tools/openAI.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import io
-from langchain.chat_models import ChatOpenAI
+from langchain.openai import ChatOpenAI
 from langchain.schema import HumanMessage
 
 # ---- Model initialization ----
